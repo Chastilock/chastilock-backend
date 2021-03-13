410faac15ec3ce3e083cfa154e377d7ca7244bf5
Havok <thetechnerd@protonmail.com>
2021-03-13 10:15:33 +0000

    Added columns but broke mitigation

--- models/OriginalLockType.js
+++ models/OriginalLockType.js
@@ -73,7 +73,115 @@ module.exports = (sequelize) => {
       Variable_Min_RandomRed: {
         type: Sequelize.INTEGER,
         allowNull: false,
-      }
+      },
+      Chance_Period: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      Cumulative: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      Multiple_Greens_Required: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      Hide_Card_Info: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      Allow_Fakes: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      Min_Fakes: {
+        type: Sequelize.INTEGER,
+        allowNull: true
+      },
+      Max_Fakes: {
+        type: Sequelize.INTEGER,
+        allowNull: true
+      },
+      Auto_Resets_Enabled: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      Reset_Frequency: {
+        type: Sequelize.INTEGER,
+        allowNull: true
+      },
+      Max_Resets: {
+        type: Sequelize.INTEGER,
+        allowNull: true
+      },
+      Checkins_Enabled: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      Checkins_Frequency: {
+        type: Sequelize.INTEGER,
+        allowNull: true
+      },
+      Checkins_Window: {
+        type: Sequelize.INTEGER,
+        allowNull: true
+      },
+      Allow_Buyout: {
+        type: Sequelize.INTEGER,
+        allowNull: true
+      },
+      Allow_Buyout: {
+        type: Sequelize.INTEGER,
+        allowNull: true
+      },
+      Start_Lock_Frozen: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      Disable_Keyholder_Decision: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      Disable_Keyholder_Decision: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      Limit_Users: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      User_Limit_Amount: {
+        type: Sequelize.INTEGER,
+        allowNull: true
+      },
+      Block_Test_Locks: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      Block_User_Rating_Enabled: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      Block_User_Rating: {
+        type: Sequelize.INTEGER,
+        allowNull: true
+      },
+      Block_Already_Locked: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      Block_Stats_Hidden: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      Only_Accept_Trusted: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      Require_DM: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      }      
     }, {sequelize}
     );
 
