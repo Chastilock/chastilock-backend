410c3c7d57f5e4d0ad0ed7cbd28d083e9715a1a1
Havok <thetechnerd@protonmail.com>
2021-03-26 17:46:14 +0000

    Fixed test (I hope)

--- helpers/validation.js
+++ helpers/validation.js
@@ -1,5 +1,3 @@
-const {User} = require('../models');
-
 function ValidateEmail(Email) {
   if (/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(Email)) {
     return true;
@@ -9,6 +7,7 @@ function ValidateEmail(Email) {
 }
 
 async function CheckUsernameAvailable(Username) {
+  const {User} = require('../models');
   const UserSearch = await User.findOne({
     where: {
       Username: Username
@@ -22,6 +21,7 @@ async function CheckUsernameAvailable(Username) {
 }
 
 async function CheckEmailAvailable(Email) {
+  const {User} = require('../models');
   const UserSearch = await User.findOne({
     where: {
       Email: Email
