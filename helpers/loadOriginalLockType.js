dd7da11dc8910db66d86c94b8d9546a615ac7827
Havok <thetechnerd@protonmail.com>
2021-04-01 22:23:30 +0100

    Worked on loading lock

---
+++ helpers/loadOriginalLockType.js
@@ -0,0 +1,15 @@
+const { OriginalLockType } = import("../models");
+async function loadOriginalLockType(CreatedLock) {
+
+    const OriginalLockID = CreatedLock.OriginalLockType_ID;
+    const LockDetails = await OriginalLockType.findByPk(OriginalLockID);
+
+    if(!LockDetails) {
+        return "Cannot find lock";
+    }
+
+
+
+
+}
+module.exports = loadOriginalLockType
--- mutations/loadLock.js
+++ mutations/loadLock.js
@@ -1,7 +1,7 @@
-const { AuthenticationError } = require('apollo-server-express');
+const { AuthenticationError, ApolloError } = require('apollo-server-express');
+const loadOriginalLockType = import('../helpers/loadOriginalLockType');
 
 async function loadLock(inputs, models, req) {
-    const validationErrors = [];
 
     if(req.AppFound === false) {
         throw new AuthenticationError("App does not exist");
@@ -16,11 +16,17 @@ async function loadLock(inputs, models, req) {
             Shared_Code: inputs.ShareCode
         }
     });
-    if(LockSearch) {
-        console.log("Lock Found")
-    } else {
-        console.log("Lock not found")
+    if(!LockSearch) {
+        throw new ApolloError("Lock not found", "404");
+    }
 
+    if(LockSearch.Disabled === true) {
+        throw new ApolloError("Lock has been disabled", "403");
+    }
+
+    if(LockSearch.OriginalLockType_ID != null) {
+        //Original lock type. Will use helper function to return the lock to keep this file neat!
+        loadOriginalLockType(LockSearch);
     }
     
 }
