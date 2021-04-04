fef6712563d582e401a036ec282243f82cae777b
Havok <thetechnerd@protonmail.com>
2021-04-04 09:47:04 +0100

    Added testtoken to seeder

--- seeders/20210313175831-example-data.js
+++ seeders/20210313175831-example-data.js
@@ -3,6 +3,7 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('Apps', [{
+        App_ID: 1,
         Name: 'Android App',
         API_Key: 'test',
         API_Secret: 'test',
@@ -10,6 +11,7 @@ module.exports = {
         updatedAt: new Date(),
       }], {});
       await queryInterface.bulkInsert('Users', [{
+        User_ID: 1,
         UUID: '42e28bdd-626d-4a1c-8aa1-a63064041c1e',
         Email: "thisisatest@example.com",
         Username: "Havok",
@@ -20,7 +22,15 @@ module.exports = {
         updatedAt: new Date(),
       }], {});
       await queryInterface.bulkInsert('Users', [{
+        User_ID: 2,
         UUID: 'fa48915c-4a9a-4eff-82d4-763e3b9608fe',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }], {});
+      await queryInterface.bulkInsert('Sessions', [{
+        User_ID: 1,
+        Token: "testtoken",
+        App_ID: 1,
         createdAt: new Date(),
         updatedAt: new Date(),
       }], {});
@@ -29,6 +39,7 @@ module.exports = {
   down: async (queryInterface, Sequelize) => {
      await queryInterface.bulkDelete('Apps', null, {});
      await queryInterface.bulkDelete('Users', null, {});
+     await queryInterface.bulkDelete('Sessions', null, {});
     
   }
 };
