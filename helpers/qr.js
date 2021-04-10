deb39d5baed1946633eeb1bc8a8662523716e4bd
Havok <thetechnerd@protonmail.com>
2021-04-10 18:16:31 +0100

    Changed QR code text to allow for future expansion

--- helpers/qr.js
+++ helpers/qr.js
@@ -1,7 +1,7 @@
 const QRCode = require('qrcode');
 
 async function QRAsDataURL(ShareCode) {
-  QRCode.toDataURL(`chastilock-${ShareCode}`, function (err, url) {
+  QRCode.toDataURL(`chastilocksharedlock-${ShareCode}`, function (err, url) {
     if(err === null) {
       return url;
     } else {
