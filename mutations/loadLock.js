2a1004fa8e6a00c30145f061d223b29a1a78206f
Havok <thetechnerd@protonmail.com>
2021-04-16 17:57:23 +0100

    Worked on loadlock

--- mutations/loadLock.js
+++ mutations/loadLock.js
@@ -37,13 +37,80 @@ async function loadLock(inputs, models, req) {
         }
     }
 
-    if(LockSearch.Allow_Fakes === true) {
+    if(inputs.Min_Fakes > 0 || inputs.Max_Fakes > 0) {
+
+        if(LockSearch.Allow_Fakes === false) {
+            validationErrors.push("Fakes are not allowed on this lock");
+        }
+
+       if(inputs.Min_Fakes > inputs.Max_Fakes) {
+        validationErrors.push("Invalid fake lock amounts");
+       }
+
+       if(inputs.Min_Fakes < LockSearch.Min_Fakes) {
+        validationErrors.push("You need more fake locks to be able to load this lock");
+       }
+
+       if(inputs.Max_Fakes > LockSearch.Max_Fakes) {
+        validationErrors.push("You need less fake locks to be able to load this lock");
+       }
+
+       if(inputs.Emergency_Keys === true) {
+        if(LockSearch.Allow_Buyout === false) {
+            validationErrors.push("You are not allowed to enable emergency keys on this lock");
+        }
+       }
+
+       if(LockSearch.Limit_Users === true) {
+           const UserLimit = LockSearch.User_Limit_Amount;
+           //TODO: Will need to exclude unlocked locks 
+           const NumOfLocksLoaded = models.LoadedLock.findAll({
+                where: {
+                    CreatedLock_ID: LockSearch.Lock_ID
+                }
+            })
+
+            if(NumOfLocksLoaded.length >= UserLimit) {
+                validationErrors.push("The keyholder has set a limit on how many of these locks that can be loaded and the limit has been reached.");
+            }
+       }
+
+       if(LockSearch.Block_Test_Locks) {
+           if(inputs.Test_Lock === true) {
+                validationErrors.push("The keyholder does not allow test locks");
+           }
+       }
+
+       if(LockSearch.Block_User_Rating_Enabled) {
+           //TODO: This needs adding once ratings are added
+       }
+
+       if(LockSearch.Block_Already_Locked) {
+           //TODO: This needs adding once unlocked locks are added
+       }
+       
+       if(LockSearch.Block_Stats_Hidden) {
+        //TODO: This needs adding once user settings are added
+        }
+
+        if(LockSearch.Only_Accept_Trusted === true) {
+            if(inputs.Trust_Keyholder === false) {
+                validationErrors.push("The keyholder requires that you trust them");
+            }
+        }
+
+
+
+
+
+
+
        
     }
     
     if(LockSearch.OriginalLockType_ID != null) {
         //Original lock type. Will use helper function to return the lock to keep this file neat!
-        loadOriginalLockType(LockSearch);
+        await loadOriginalLockType(LockSearch);
     }
     
 }
