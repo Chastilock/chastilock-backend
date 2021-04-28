50d4bb0f97238fd7fb968a0494a138b0eec04870
SirHavok <thetechnerd@protonmail.com>
2021-04-28 22:27:01 +0100

    Worked on loading lock logic

--- helpers/loadOriginalLockType.js
+++ helpers/loadOriginalLockType.js
@@ -1,4 +1,6 @@
+const { UserInputError } = require("apollo-server-errors");
 const { OriginalLockType } = require("../models");
+const { RandomInt, SplitNumberIntoRand } = require('./random');
 
 async function loadOriginalLockType(CreatedLock) {
 
@@ -8,8 +10,44 @@ async function loadOriginalLockType(CreatedLock) {
     const LockDetails = await OriginalLockType.findByPk(OriginalLockID);
 
     if(!LockDetails) {
-        console.log("Cannot find lock");
-        Error.push("Cannot find lock");
+        Errors.push("Cannot find lock");
     }
+
+    const Reds = RandomInt(LockDetails.Variable_Min_Reds, LockDetails.Variable_Max_Reds);
+    const Greens = RandomInt(LockDetails.Variable_Min_Greens, LockDetails.Variable_Min_Greens);
+    const Stickies = RandomInt(LockDetails.Variable_Min_Stickies, LockDetails.Variable_Max_Stickies);
+    const Resets = RandomInt(LockDetails.Variable_Min_Resets, LockDetails.Variable_Max_Resets);
+    const Doubles = RandomInt(LockDetails.Variable_Min_Doubles, LockDetails.Variable_Max_Doubles);
+
+    const TotalAddReds = RandomInt(LockDetails.Variable_Min_AddRed, LockDetails.Variable_Max_AddRed);
+    const TotalRemoveReds = RandomInt(LockDetails.Variable_Min_RemoveRed, LockDetails.Variable_Max_RemoveRed);
+    const TotalRandomReds = RandomInt(LockDetails.Variable_Min_RandomRed, LockDetails.Variable_Max_RandomRed);
+
+    const RandomSplit = SplitNumberInto2Rand(TotalRandomReds);
+
+    TotalAddReds =+ RandomSplit.Num1
+    TotalRemoveReds =+ RandomSplit.Num2
+
+    const AddRedSplit = SplitNumberInto3Rand(TotalAddReds);
+
+    const Add1 = AddRedSplit.Num1
+    const Add2 = AddRedSplit.Num2
+    const Add3 = AddRedSplit.Num3
+
+    const RemoveRedSplit = SplitNumberInto2Rand(TotalRemoveReds);
+
+    const Remove1 = RemoveRedSplit.Num1
+    const Remove2 = RemoveRedSplit.Num2
+
+    
+
+
+
+
+    if(Errors.length) {
+      throw new UserInputError("Unable to load lock", {
+        invalidArgs: Errors
+      })
+    } 
 }
 module.exports = loadOriginalLockType
--- helpers/random.js
+++ helpers/random.js
@@ -4,4 +4,31 @@ function RandomInt(min, max) {
     )
 }
 
-module.RandomInt = RandomInt
+function SplitNumberInto2Rand(Input) {  
+  const Num1 = RandomInt(0, Input);
+  const Num2 = Input - Num1;
+
+  return {
+    Num1,
+    Num2
+  }
+}
+
+function SplitNumberInto3Rand(Input) {  
+  const Num1 = RandomInt(0, Input);
+  const Remaining = Input - Num1;
+  const Num2 = RandomInt(0, Remaining);
+  const Num3 = Input - (Num1 + Num2)
+
+  return {
+    Num1,
+    Num2,
+    Num3
+  }
+}
+
+module.exports = {
+  RandomInt,
+  SplitNumberInto2Rand,
+  SplitNumberInto3Rand
+}
