e5cfa44573ce02119d316052c185987d0ff8b3e6
Havok <thetechnerd@protonmail.com>
2021-04-29 21:11:08 +0100

    Added more to loadlock

--- helpers/loadOriginalLockType.js
+++ helpers/loadOriginalLockType.js
@@ -1,6 +1,6 @@
 const { UserInputError } = require("apollo-server-errors");
-const { OriginalLockType } = require("../models");
-const { RandomInt, SplitNumberInto2Rand, SplitNumberInto3Rand } = require('./random');
+const { OriginalLockType, LoadedOriginalLock } = require("../models");
+const { RandomInt, SplitNumberInto2Rand, SplitNumberInto3Rand, SplitNumberInto5Rand } = require('./random');
 
 async function loadOriginalLockType(CreatedLock) {
 
@@ -18,6 +18,8 @@ async function loadOriginalLockType(CreatedLock) {
     const Stickies = RandomInt(LockDetails.Variable_Min_Stickies, LockDetails.Variable_Max_Stickies);
     const Resets = RandomInt(LockDetails.Variable_Min_Resets, LockDetails.Variable_Max_Resets);
     const Doubles = RandomInt(LockDetails.Variable_Min_Doubles, LockDetails.Variable_Max_Doubles);
+    const Freezes = RandomInt(LockDetails.Variable_Min_Freezes, LockDetails.Variable_Max_Freezes);
+
 
     const TotalAddReds = RandomInt(LockDetails.Variable_Min_AddRed, LockDetails.Variable_Max_AddRed);
     const TotalRemoveReds = RandomInt(LockDetails.Variable_Min_RemoveRed, LockDetails.Variable_Max_RemoveRed);
@@ -39,15 +41,37 @@ async function loadOriginalLockType(CreatedLock) {
     const Remove1 = RemoveRedSplit.Num1
     const Remove2 = RemoveRedSplit.Num2
 
+    const SplitRandom = SplitNumberInto5Rand(TotalRandomReds);
+
+    Add1 = Add1 + SplitRandom.Num1
+    Add2 = Add2 + SplitRandom.Num2
+    Add3 = Add3 + SplitRandom.Num3
+    Remove1 = Remove1 + SplitRandom.Num4
+    Remove2 = Remove2 + SplitRandom.Num5
     
-
-
-
 
     if(Errors.length) {
       throw new UserInputError("Unable to load lock", {
         invalidArgs: Errors
       })
-    } 
+    }
+    
+    const OriginalLockRecord = await LoadedOriginalLock.create({
+      Remaining_Red: Reds,
+      Remaining_Green: Greens,
+      Found_Green: 0,
+      Remaining_Sticky: Stickies,
+      Remaining_Add1: Add1,
+      Remaining_Add2: Add2,
+      Remaining_Add3: Add3,
+      Remaining_Remove1: Remove1,
+      Remaining_Remove2: Remove2,
+      Remaining_Freeze: Freezes,
+      Remaining_Double: Doubles,
+      Remaining_Reset: Resets,
+      Cumalative: LockDetails.Cumulative
+    })
+
+    return OriginalLockRecord
 }
 module.exports = loadOriginalLockType
--- helpers/random.js
+++ helpers/random.js
@@ -27,8 +27,29 @@ function SplitNumberInto3Rand(Input) {
   }
 }
 
+function SplitNumberInto5Rand(Input) {  
+  const Num1 = RandomInt(0, Input);
+  const Remaining = Input - Num1;
+  const Num2 = RandomInt(0, Remaining);
+  const Remaining = Input - (Num1 + Num2);
+  const Num3 = RandomInt(0, Remaining);
+  const Remaining = Input - (Num1 + Num2 + Num3);
+  const Num4 = RandomInt(0, Remaining);
+  const Num5 = Input - (Num1 + Num2 + Num3 + Num4)
+
+
+  return {
+    Num1,
+    Num2,
+    Num3,
+    Num4,
+    Num5
+  }
+}
+
 module.exports = {
   RandomInt,
   SplitNumberInto2Rand,
-  SplitNumberInto3Rand
+  SplitNumberInto3Rand,
+  SplitNumberInto5Rand
 }
