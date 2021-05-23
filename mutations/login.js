ac3f8afd586efb877b70826e7d8cbfac193b761f
Havok <thetechnerd@protonmail.com>
2021-05-23 13:50:24 +0100

    Require email to be validated to login

--- mutations/login.js
+++ mutations/login.js
@@ -24,6 +24,10 @@ async function login({ Username, Password }, models, req) {
   if(Bcypt.compareSync(Password, hash) === false) {
     throw new AuthenticationError("Username or password is incorrect");
   }
+
+  if(userSearch.Email_Validated === false) {
+    throw new AuthenticationError("You need to validate your email adddress in order to login");
+  }
   
   return models.Session.create({
     User_ID: userSearch.User_ID,
