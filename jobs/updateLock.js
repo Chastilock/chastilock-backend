3da9350f894756e190ff3ae451001e3a6e2afb49
Havok <thetechnerd@protonmail.com>
2021-06-19 22:38:39 +0100

    Worked on updatelock

--- jobs/updateLock.js
+++ jobs/updateLock.js
@@ -11,26 +11,37 @@ const UpdateLock = async function() {
     CurrentlyRunningLocks.forEach(Lock => {
         console.log(`Updating LoadedLock: ${Lock.LoadedLock_ID}`);
 
+        const ThisLock = await LoadedLock.findOne({
+            where: {
+                LoadedLock_ID: Lock.LoadedLock_ID
+            }
+        });
+
         //Check if the lock is frozen and update the loaded lock table as such
 
         const CurrentDateAndTime = new Date();
         const Frozen = Lock.Current_Freeze_ID;
 
         if(Frozen != null) {
+            console.log(`Lock ${Lock.LoadedLock_ID} is currently frozen. Checking...`)
             const FreezeRecord = await Freeze.findOne({
                 where: {
                     Freeze_ID: Frozen
                 }
             });
-
             if (FreezeRecord === null) {
-                //Need to remove the freeze ID from the LoadedLock table
+                console.log(`Freeze doesn't exist. Fixing...`)
+                ThisLock.set({Current_Freeze_ID: null})
             } else {
                 //Check if the freeze has ended
                 if(FreezeRecord.EndTime != null && FreezeRecord.EndTime < CurrentDateAndTime) {
-                    //Need to remove the freeze ID from the LoadedLock table
+                    console.log(`Freeze is scheduled to have ended already. Removing Freeze...`)
+                    ThisLock.set({Current_Freeze_ID: null})
                 }
             }
+        } else {
+            console.log(`Lock ${Lock.LoadedLock_ID} is not currently frozen, but should it be... 👿`);
+               
         }
 
         //Check if a autoreset should have happened and action it if it should have
