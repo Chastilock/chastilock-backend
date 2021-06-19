a38bef62bb0c751c4a1e86fe42560a931362317e
Havok <thetechnerd@protonmail.com>
2021-06-19 10:26:07 +0100

    Re-added the columns using interactive migrations

---
+++ migrations/20210619091114-LockChances.js
@@ -0,0 +1,45 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return [
+      await queryInterface.addColumn("LoadedOriginalLocks", "Chance_Period", {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      }),
+      await queryInterface.addColumn("LoadedOriginalLocks", "Chances_Remaining", {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      }),
+      await queryInterface.addColumn("LoadedOriginalLocks", "Chances_Last_Calulated", {
+        type: Sequelize.DATE,
+        allowNull: false
+      }),
+      await queryInterface.addColumn("LoadedOriginalLocks", "Auto_Resets_Paused", {
+        type: Sequelize.BOOLEAN,
+        allowNull: false
+      }),
+      await queryInterface.addColumn("LoadedOriginalLocks", "Auto_Resets_Frequency", {
+        type: Sequelize.INTEGER,
+        allowNull: true
+      }),
+      await queryInterface.addColumn("LoadedOriginalLocks", "Last_Auto_Reset", {
+        type: Sequelize.DATE,
+        allowNull: true
+      }),
+
+    ]
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return [
+      await queryInterface.removeColumn("LoadedOriginalLocks", "Chance_Period"),
+      await queryInterface.removeColumn("LoadedOriginalLocks", "Chances_Remaining"),
+      await queryInterface.removeColumn("LoadedOriginalLocks", "Chances_Last_Calulated"),
+      await queryInterface.removeColumn("LoadedOriginalLocks", "Auto_Resets_Paused"),
+      await queryInterface.removeColumn("LoadedOriginalLocks", "Auto_Resets_Frequency"),
+      await queryInterface.removeColumn("LoadedOriginalLocks", "Last_Auto_Reset"),
+
+    ]
+  }
+};
