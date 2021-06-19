3893d5660e0dbc7ce2a2221032811ad92e921cd4
Havok <thetechnerd@protonmail.com>
2021-06-19 21:46:23 +0100

    Moved columns round in LoadedOriginalLocks

--- migrations/20210619091114-LockChances.js
+++ migrations/20210619091114-LockChances.js
@@ -5,31 +5,38 @@ module.exports = {
     return [
       await queryInterface.addColumn("LoadedOriginalLocks", "Chance_Period", {
         type: Sequelize.INTEGER,
-        allowNull: false
+        allowNull: false,
+        after: "Hide_Card_Info"
       }),
       await queryInterface.addColumn("LoadedOriginalLocks", "Chances_Remaining", {
         type: Sequelize.INTEGER,
-        allowNull: false
+        allowNull: false,
+        after: "Chance_Period"
       }),
       await queryInterface.addColumn("LoadedOriginalLocks", "Chances_Last_Calulated", {
         type: Sequelize.DATE,
-        allowNull: false
+        allowNull: false,
+        after: "Chances_Remaining"
       }),
       await queryInterface.addColumn("LoadedOriginalLocks", "Auto_Resets_Paused", {
         type: Sequelize.BOOLEAN,
-        allowNull: false
+        allowNull: false,
+        after: "Chances_Last_Calulated"
       }),
       await queryInterface.addColumn("LoadedOriginalLocks", "Auto_Resets_Frequency", {
         type: Sequelize.INTEGER,
-        allowNull: true
+        allowNull: true,
+        after: "Auto_Resets_Paused"
       }),
       await queryInterface.addColumn("LoadedOriginalLocks", "Auto_Resets_Time_Left", {
         type: Sequelize.INTEGER,
-        allowNull: true
+        allowNull: true,
+        after: "Auto_Resets_Frequency"
       }),
       await queryInterface.addColumn("LoadedOriginalLocks", "Last_Auto_Reset", {
         type: Sequelize.DATE,
-        allowNull: true
+        allowNull: true,
+        after: "Auto_Resets_Time_Left"
       })
     ]
   },
