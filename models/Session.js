ab24a1ffd64189b041e68fe736e324435cfb979c
Ed <>
2021-06-20 10:58:26 -0400

    Added migration to add necessary fields

--- models/Session.js
+++ models/Session.js
@@ -18,7 +18,7 @@ module.exports = (sequelize) => {
         allowNull: false,
       },
       App_ID: {
-        type: Sequelize.STRING,
+        type: Sequelize.INTEGER,
         allowNull: false,
       }
       
