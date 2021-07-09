bdd76f52c3f82817d5652f2b86d775e2df21252c
Ed <>
2021-07-09 13:59:36 -0400

    Modified fxn hardResetLock to update old deck with new card information and preserve all other fields

--- helpers/lockModifyingFunctions.js
+++ helpers/lockModifyingFunctions.js
@@ -6,7 +6,9 @@ const { RandomInt } = require('./random')
 /**
  * This function does a hard reset, that is, it resets ALL cards based on the original distribution
  * specified in the original CreatedLock template.  A hard reset can be either an auto-reset or a keyholder
- * reset, but not a card reset, which is a soft reset
+ * reset, but not a card reset, which is a soft reset.  This function uses loadOriginalLockType to create a
+ * new deck (LoadedOriginalLock) and then copies the card information from the new deck into the old deck without
+ * modifying other fields, and then cleans up by saving the modified old deck and deleting the new deck.
  * @param { LoadedLock } lock 
  * @returns { void }
  */
@@ -20,11 +22,6 @@ async function hardResetLock(lock) {
     const createdLock = await CreatedLock.findByPk(lock.CreatedLock_ID);
 
     if (lock.Original_Lock_Deck) { // variable/card lock
-
-        // TODO: replace next 2 lines with appropriate code once chance code is written
-        //lock.Chances = 1
-        //lock.Last_Chance_Time = Date.now()
-
         // get old deck
         /** @type LoadedOriginalLock */
         oldDeck = await LoadedOriginalLock.findByPk(lock.Original_Lock_Deck)
@@ -32,12 +29,27 @@ async function hardResetLock(lock) {
         // create new deck
         /** @type LoadedOriginalLock */
         newDeck = await loadOriginalLockType(createdLock)
-        
-        lock.Original_Lock_Deck = newDeck.Original_Loaded_ID
-        await lock.save() // must save lock b4 destroying oldDeck because of constraint
 
-        // delete old deck from DB
-        await oldDeck.destroy()
+        // copy card info from new deck to old deck, save the old deck and then delete the new deck 
+        oldDeck.Remaining_Red = newDeck.Remaining_Red
+        oldDeck.Remaining_Green = newDeck.Remaining_Green
+        oldDeck.Found_Green = 0
+        oldDeck.Remaining_Sticky = newDeck.Remaining_Sticky
+        oldDeck.Remaining_Add1 = newDeck.Remaining_Add1
+        oldDeck.Remaining_Add2 = newDeck.Remaining_Add2
+        oldDeck.Remaining_Add3 = newDeck.Remaining_Add3
+        oldDeck.Remaining_Remove1 = newDeck.Remaining_Remove1
+        oldDeck.Remaining_Remove2 = newDeck.Remaining_Remove2
+        oldDeck.Remaining_Freeze = newDeck.Remaining_Freeze
+        oldDeck.Remaining_Double = newDeck.Remaining_Double
+        oldDeck.Remaining_Reset = newDeck.Remaining_Reset
+        oldDeck.Remaining_GoAgain = newDeck.Remaining_GoAgain
+        oldDeck.Chances_Remaining = 1
+        oldDeck.Chances_Last_Calulated = Date.now()
+        // other fields in oldDeck may have been modified from original settings, so not copied.
+        await oldDeck.save()
+        // delete new deck from DB
+        await newDeck.destroy()
     } else if (lock.Timed_Unlock_Time) { 
         /** @type {TimerLockType} */
         timed = await TimerLockType.findByPk(createdLock.TimerLockType_ID);
