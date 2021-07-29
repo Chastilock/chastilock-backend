1df76ad898b84bff9e39fc7be3db91b2a3dc9896
SirHavok <thetechnerd@protonmail.com>
2021-07-29 23:02:23 +0100

    Worked more on bots

--- migrations/20210723183157-BotsTable.js
+++ migrations/20210723183157-BotsTable.js
@@ -19,6 +19,10 @@ module.exports = {
       Bot_Difficulty: {
         type: Sequelize.STRING,
         allowNull: false,
+      },
+      Max_Time_Before_Updates: {
+        type: Sequelize.INTEGER,
+        allowNull: false
       }
     });  
   },
