6192e2f6cf0974fd78e98bf5cb81ca26d53b909f
Havok <thetechnerd@protonmail.com>
2021-12-18 14:59:28 +0000

    Re-used password reset record if it hasn't expired

--- mutations/requestPasswordChange.js
+++ mutations/requestPasswordChange.js
@@ -1,7 +1,8 @@
 const { AuthenticationError, UserInputError, ApolloError } = require('apollo-server-express');
 const { ValidateEmail } = require('../helpers/validation');
 const { v4: uuidv4 } = require('uuid');
-const sendemail = require("../helpers/email.js")
+const sendemail = require("../helpers/email.js");
+const { Op } = require('sequelize');
 
 async function requestPasswordChange(inputs, models, req) {
 
@@ -21,15 +22,35 @@ async function requestPasswordChange(inputs, models, req) {
 
     if(UserSearch) {
 
-        const Expiry = new Date();
-        Expiry.setHours(Expiry.getHours() + 2);
+        const now = new Date();
 
-        const record = await models.PasswordReset.create({
-            User_ID: UserSearch.User_ID,
-            Code: uuidv4(),
-            Expires: Expiry
-        });
+        const PasswordResetSearch = await models.PasswordReset.findOne({
+            where: {
+                [Op.and]: {
+                    User_ID: {
+                        [Op.eq]: UserSearch.User_ID
+                    },
+                    Expires: {
+                        [Op.gt]: now
+                    }
+                }
+            }
+        })
 
+        let record = "";
+
+        if(PasswordResetSearch) {
+            record = PasswordResetSearch
+        } else {
+            const Expiry = new Date();
+            Expiry.setHours(Expiry.getHours() + 2);
+            
+            record = await models.PasswordReset.create({
+                User_ID: UserSearch.User_ID,
+                Code: uuidv4(),
+                Expires: Expiry
+            });
+        }
         sendemail(UserSearch.User_ID, "ForgottenPassword", {code: record.Code});
         return record;
     } else {
