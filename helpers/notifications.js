bcbba69ed508f3e9c9e77510556e544b3d6230ad
Havok <thetechnerd@protonmail.com>
2022-06-25 23:55:01 +0100

    Start migrating to firebase messaging

--- helpers/notifications.js
+++ helpers/notifications.js
@@ -21,14 +21,8 @@ async function NotifyAllUsers(body, data) {
 
     for(let Token of Tokens) {
         
-        if(!checkNotificationToken(Token)) {
-            console.error(`Push token ${Token} is not a valid Expo push token`);
-            continue;
-        }
-
         messages.push({
-            to: Token,
-            sound: 'default',
+            token: Token,
             body,
             data
         })
@@ -61,26 +55,11 @@ async function addMessagesForSingleUser(UserID, messages, body, data) {
 
 
 function sendMessages(messages) {
-    let chunks = expo.chunkPushNotifications(messages);
-    let tickets = [];
-    (async () => {
-    // Send the chunks to the Expo push notification service. There are
-    // different strategies you could use. A simple one is to send one chunk at a
-    // time, which nicely spreads the load out over time:
-        for (let chunk of chunks) {
-            try {
-                let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
-                console.log(ticketChunk);
-                tickets.push(...ticketChunk);
-                // NOTE: If a ticket contains an error code in ticket.details.error, you
-                // must handle it appropriately. The error codes are listed in the Expo
-                // documentation:
-                // https://docs.expo.io/push-notifications/sending-notifications/#individual-errors
-            } catch (error) {
-                console.error(error);
-            }
-        }
-    })();
+    //Need to split the messages into chuncks of no more than 500
+
+    getMessaging().sendAll(messages).then((response) => {
+        console.log(response.successCount + ' messages were sent successfully');
+    });
 }
 
 function checkNotificationToken(Token) {
